2ebca85e7bffcb96d8e6a374d0f3d7da2973ee4c
= <=>
2022-10-19 12:26:18 +0300

    Section Destination, button+slider done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    center: true,
-    loop: true,
-    margin: 30,
-    items: 3,
+
+
+const owl = $('.owl-carousel');
+owl.owlCarousel({
+  center: true,
+  loop: true,
+  margin: 30,
+  items: 3,
+  
+});
+
+
+$('.slider__btn--prev').click(function() {
     
-  });
-}); 
+    owl.trigger('prev.owl.carousel');
+})
+
+
+$('.slider__btn--next').click(function() {
+  owl.trigger('next.owl.carousel');
+}) 
