a3685b9aec921e85caf1f865a39c5f49619ef6f1
= <=>
2022-11-03 10:32:02 +0300

    mobile version header, nav

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
+// ** Carousel**
 
-
-const owl = $('.owl-carousel');
+const owl = $(".owl-carousel");
 owl.owlCarousel({
   center: true,
   loop: true,
   margin: 30,
   items: 3,
-  
 });
 
+$(".slider__btn--prev").click(function () {
+  owl.trigger("prev.owl.carousel");
+});
 
-$('.slider__btn--prev').click(function() {
-    
-    owl.trigger('prev.owl.carousel');
-})
+$(".slider__btn--next").click(function () {
+  owl.trigger("next.owl.carousel");
+});
 
+// * Nav icon*
 
-$('.slider__btn--next').click(function() {
-  owl.trigger('next.owl.carousel');
-}) 
+const navBtn = document.querySelector(".nav__toggle");
+const menuIcon = document.querySelector(".menu-icon");
+
+navBtn.onclick = function () {
+  menuIcon.classList.toggle("menu-icon-active");
+};
